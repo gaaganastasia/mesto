fcdd17721724fcf5bd6303e681c53ef37b46440d
Anastasia Gaag <gaag.a.a@mail.ru>
2020-11-01 18:30:07 +0700

    правки

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -45,6 +44,5 @@ module.exports = {
       template: 'src/index.html'
     }),
     new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin()
   ]
 };
